05bf0eb8ee05693f2b35ccebf20496d618085543
Yasassri Ratnayake <ycrnet@gmail.com>
2016-11-02 12:02:52 +0530

    Adding more tests

---
+++ src/test/java/org/wso2/apim/deployment/api/creation/ApplicationCreationTestCase.java
@@ -0,0 +1,56 @@
+package org.wso2.apim.deployment.api.creation;
+
+import org.testng.ITestContext;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Factory;
+import org.testng.annotations.Test;
+import org.wso2.apim.bean.APIThrottlingTier;
+import org.wso2.apim.clients.APIPublisherRestClient;
+import org.wso2.apim.clients.APIStoreRestClient;
+import org.wso2.apim.deployment.base.APIMBaseTest;
+import org.wso2.carbon.automation.engine.context.TestUserMode;
+import org.wso2.carbon.automation.test.utils.http.client.HttpResponse;
+
+/**
+ * Created by yasassri on 11/2/16.
+ */
+public class ApplicationCreationTestCase extends APIMBaseTest {
+    private APIPublisherRestClient apiPublisher;
+    private APIStoreRestClient apiStore;
+    private String appName = "sample-application1";
+
+    @Factory(dataProvider = "userModeDataProvider")
+    public ApplicationCreationTestCase(TestUserMode userMode) {
+        this.userMode = userMode;
+    }
+
+    @BeforeClass(alwaysRun = true)
+    public void setEnvironment(ITestContext ctx) throws Exception {
+        super.init(userMode);
+        //String publisherURLHttp = getPublisherURLHttp();
+        //String storeURLHttp = getStoreURLHttp();
+
+        apiStore = new APIStoreRestClient(storeURL);
+        apiPublisher = new APIPublisherRestClient(publisherURL);
+
+        apiPublisher.login(user.getUserName(), user.getPassword());
+        apiStore.login(user.getUserName(), user.getPassword());
+
+    }
+
+
+    @Test(groups = {"wso2.am"}, description = "Sample Application Creation")
+    public void testApplicationCreation() throws Exception {
+        HttpResponse serviceResponse = apiStore.addApplication(appName, APIThrottlingTier.UNLIMITED.getState(), "", "this-is-test");
+        verifyResponse(serviceResponse);
+    }
+
+    @DataProvider
+    public static Object[][] userModeDataProvider() {
+        return new Object[][]{
+                new Object[]{TestUserMode.SUPER_TENANT_ADMIN},
+                //new Object[]{TestUserMode.TENANT_ADMIN},
+        };
+    }
+}
